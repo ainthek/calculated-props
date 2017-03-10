ba64f9da0d47de13954a16b31359ee964d0850ff
Martin Marko <marcus@gratex.com>
2017-03-10 15:56:35 +0100

    sugaized

---
+++ test/05b-dojox.mvc.computed-sugar.js
@@ -0,0 +1,57 @@
+/*global describe:true,it:true, after:true,before:true,afterEach:true,beforeEach:true */
+var assert = require("assert");
+var drequire = require("drequire")();
+var getStateful = drequire("dojox/mvc/getStateful");
+var computed = drequire("dojox/mvc/computed");
+var at = drequire("dojox/mvc/at");
+
+
+describe.only("a=b/10; b=a*10", function() {
+
+    function sugar(plain) {
+        var stateful = getStateful(plain);
+        for (let p in plain) {
+            if (typeof plain[p] === "function") {
+                let funct = plain[p];
+                let ats = args(funct).map((pn) => at(stateful, pn));
+                computed.apply(null, [stateful, p, funct].concat(ats));
+            }
+        }
+        return stateful;
+        function args(funct) {
+            return require("parse-function")({})
+                .parse(funct).args;
+        }
+    }
+
+    it("dojox/mvc/computed - syntactic sugar", function() {
+        var o = sugar({
+            a: (b) => b / 10,
+            b: (a) => a * 10,
+            c: (a, b) => a + b
+        });
+
+        function changeA(value) {
+            o.set("a", value);
+        }
+
+        function changeB(value) {
+            o.set("b", value);
+        }
+
+        assert(isNaN(o.get("a"))); //already getter fails
+        assert(isNaN(o.get("b")));
+
+        changeA(5);
+        assert.equal(o.get("a"), 5);
+        assert.equal(o.get("b"), 50);
+
+        changeB(10);
+        assert.equal(o.get("a"), 1);
+        assert.equal(o.get("b"), 10);
+
+        //
+        assert.equal(o.get("c"), 11);
+
+    });
+});
